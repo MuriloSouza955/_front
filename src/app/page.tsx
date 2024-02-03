778bf49f77afa51d9cea48fbf84d44638f286902
Fabio S. Takaki <fabio.stakaki@gmail.com>
2024-02-03 18:30:36 -0300

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,111 +3,96 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+     <ul role="list" className="divide-y divide-gray-100">
+  <li className="flex justify-between gap-x-6 py-5">
+    <div className="flex min-w-0 gap-x-4">
+      <Image alt="Logo Controller Solar" width={12} height={12} className="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" />
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold leading-6 text-gray-900">Leslie Alexander</p>
+        <p className="mt-1 truncate text-xs leading-5 text-gray-500">leslie.alexander@example.com</p>
+      </div>
+    </div>
+    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p className="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
+      <p className="mt-1 text-xs leading-5 text-gray-500">Last seen <time dateTime="2023-01-23T13:23Z">3h ago</time></p>
+    </div>
+  </li>
+  <li className="flex justify-between gap-x-6 py-5">
+    <div className="flex min-w-0 gap-x-4">
+      <Image alt="Logo Controller Solar" width={12} height={12} className="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"  />
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold leading-6 text-gray-900">Michael Foster</p>
+        <p className="mt-1 truncate text-xs leading-5 text-gray-500">michael.foster@example.com</p>
+      </div>
+    </div>
+    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p className="text-sm leading-6 text-gray-900">Co-Founder / CTO</p>
+      <p className="mt-1 text-xs leading-5 text-gray-500">Last seen <time dateTime="2023-01-23T13:23Z">3h ago</time></p>
+    </div>
+  </li>
+  <li className="flex justify-between gap-x-6 py-5">
+    <div className="flex min-w-0 gap-x-4">
+      <Image alt="Logo Controller Solar" width={12} height={12} className="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"  />
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold leading-6 text-gray-900">Dries Vincent</p>
+        <p className="mt-1 truncate text-xs leading-5 text-gray-500">dries.vincent@example.com</p>
+      </div>
+    </div>
+    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p className="text-sm leading-6 text-gray-900">Business Relations</p>
+      <div className="mt-1 flex items-center gap-x-1.5">
+        <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+          <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
         </div>
+        <p className="text-xs leading-5 text-gray-500">Online</p>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+    </div>
+  </li>
+  <li className="flex justify-between gap-x-6 py-5">
+    <div className="flex min-w-0 gap-x-4">
+      <Image alt="Logo Controller Solar" width={12} height={12} className="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"  />
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold leading-6 text-gray-900">Lindsay Walton</p>
+        <p className="mt-1 truncate text-xs leading-5 text-gray-500">lindsay.walton@example.com</p>
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    </div>
+    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p className="text-sm leading-6 text-gray-900">Front-end Developer</p>
+      <p className="mt-1 text-xs leading-5 text-gray-500">Last seen <time dateTime="2023-01-23T13:23Z">3h ago</time></p>
+    </div>
+  </li>
+  <li className="flex justify-between gap-x-6 py-5">
+    <div className="flex min-w-0 gap-x-4">
+      <Image alt="Logo Controller Solar" width={12} height={12} className="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"  />
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold leading-6 text-gray-900">Courtney Henry</p>
+        <p className="mt-1 truncate text-xs leading-5 text-gray-500">courtney.henry@example.com</p>
       </div>
+    </div>
+    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p className="text-sm leading-6 text-gray-900">Designer</p>
+      <p className="mt-1 text-xs leading-5 text-gray-500">Last seen <time dateTime="2023-01-23T13:23Z">3h ago</time></p>
+    </div>
+  </li>
+  <li className="flex justify-between gap-x-6 py-5">
+    <div className="flex min-w-0 gap-x-4">
+      <Image alt="Logo Controller Solar" width={12} height={12} className="h-12 w-12 flex-none rounded-full bg-gray-50" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"  />
+      <div className="min-w-0 flex-auto">
+        <p className="text-sm font-semibold leading-6 text-gray-900">Tom Cook</p>
+        <p className="mt-1 truncate text-xs leading-5 text-gray-500">tom.cook@example.com</p>
+      </div>
+    </div>
+    <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
+      <p className="text-sm leading-6 text-gray-900">Director of Product</p>
+      <div className="mt-1 flex items-center gap-x-1.5">
+        <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+          <div className="h-1.5 w-1.5 rounded-full bg-emerald-500"></div>
+        </div>
+        <p className="text-xs leading-5 text-gray-500">Online</p>
+      </div>
+    </div>
+  </li>
+</ul>
     </main>
   );
 }
