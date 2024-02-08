e0f7d3d79d89ae365398be23eb68cd06d7098643
MuriloSouza955 <souza.murilo@gmail.com>
2024-02-07 21:48:00 -0300

    Validacao de email

--- src/app/components/Cadastro.tsx
+++ src/app/components/Cadastro.tsx
@@ -1,40 +1,75 @@
+"use client";
 import { useState } from "react";
-
 import "./style.css";
+import { validateEmail } from "../../utils/validateCadastro";
 
-export default function Cadastro(){
-    return (
-        <div className="container-login">
-          <div className="wrap-login">
-            <form className="login-form">
+export default function Cadastro() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [qtdeKW, setQtdeKW] = useState("");
+  const [emailErr, setEmailErr] = useState(false);
 
-              <div className="wrap-input">
-                <input className="input" type="name"/>
-                <span className="focus-input" data-placeholder="Nome completo"></span>
-              </div>
-
-              <div className="wrap-input">
-                <input className="input" type="email"></input>
-                <span className="focus-input" data-placeholder="Email"></span>
-              </div>
-              
-              <div className="wrap-input">
-                <input className="input" type="number"/>
-                <span className="focus-input" data-placeholder="Quantidade de KW"></span>
-              </div>
-
-              <div className="container-login-form-btn">
-                <button className="login-form-btn">Enviar</button>
-              </div>
-
-              <div className="text-center">
-                <span className="txt1">Não possui conta? </span>
-                <a className="txt2" href="#">
-                  Criar conta
-                </a>
-              </div>
-            </form>
+  const validate = () => {
+    if (!validateEmail.test(email)) {
+      setEmailErr(true);
+    } else {
+      setEmailErr(false);
+    }
+  };
+  return (
+    <div className="container-login">
+      <div className="wrap-login">
+        <form className="login-form">
+          <div className="wrap-input">
+            <input
+              className={nome !== "" ? "has-val input" : "input"}
+              type="name"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+            />
+            <span
+              className="focus-input"
+              data-placeholder="Nome completo"
+            ></span>
           </div>
-        </div>
-    );
+
+          <div className="wrap-input">
+            <input
+              className={email !== "" ? "has-val input" : "input"}
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {emailErr && <p>Digite um email válido</p>}
+
+            <span className="focus-input" data-placeholder="Email"></span>
+          </div>
+
+          <div className="wrap-input">
+            <input
+              className={qtdeKW !== "" ? "has-val input" : "input"}
+              type="number"
+              value={qtdeKW}
+              onChange={(e) => setQtdeKW(e.target.value)}
+            />
+            <span
+              className="focus-input"
+              data-placeholder="Quantidade de KW"
+            ></span>
+          </div>
+
+          <div className="container-login-form-btn">
+            <button className="login-form-btn">Enviar</button>
+          </div>
+
+          <div className="text-center">
+            <span className="txt1">Não possui conta? </span>
+            <a className="txt2" href="#">
+              Criar conta
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
